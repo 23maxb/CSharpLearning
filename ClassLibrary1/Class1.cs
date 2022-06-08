1e809afbd1dd7601c7fd1f9a1c33186c8ef48c00
Max Blennemann <23maxb@students.harker.org>
2022-06-08 06:06:56 -0700

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        /**
+         * Runs the thing
+         */
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
