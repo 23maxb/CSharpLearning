de7e47d619deda8a101495324a7ca35cfa071c55
Max Blennemann <23maxb@students.harker.org>
2022-06-08 18:54:56 -0700

    trying out new class stuff

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -3,14 +3,45 @@
 
 namespace ClassLibrary1
 {
-    public class Class1
+    public class main
     {
         /**
          * Runs the thing
          */
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            guessingGame();
         }
+
+        public static void guessingGame()
+        {
+            var real = roll(100);
+            for (;;)
+            {
+                var guess = -1;
+                Console.WriteLine("Guess (between 1 - 100):");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess > real)
+                    Console.WriteLine("Guess too high");
+                else if (guess < real)
+                    Console.WriteLine("Guess too low");
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    return;
+                }
+            }
+        }
+
+        public static int roll(int i)
+        {
+            return (int)((new Random().NextDouble()) * i) + 1;
+        }
+    }
+
+    public class sorting()
+    {
+        public 
+        
     }
 }
